eaca7cf4bcebd0cefae53c2443a4bf5962bcbff9
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-03 13:31:38 -0700

    skip make check on linux-ppc64le and linux-aarc64

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ fi
     --disable-dependency-tracking \
     --prefix=${PREFIX}
 if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 0 ]]; then
-    make -j${CPU_COUNT} check
+    if [[ ${target_platform} != "linux-ppc64le" ]] && [[ ${target_platform} != "linux-aarch64" ]]; then
+        make -j${CPU_COUNT} check
+    fi
 fi
 make -j${CPU_COUNT} install
